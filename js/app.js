e9e4fa64334e13f8a8b06de07933223ed51b0076
octaviolan <octavio.olan25@gmail.com>
2022-07-22 10:02:54 -0500

    Imagen, boton siguiente y array

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const testimonials = [
+  {
+    id: 1,
+    name: "Tanya Sinclair",
+    job: "UX Engineer",
+    image: "../images/image-tanya.jpg",
+    description: "I’ve been interested in coding for a while but never taken the jump, until now.I couldn’t recommend this course enough.I’m now in the job of my dreams and so excited about the future.",
+  },
+  {
+    id: 2,
+    name: "John Tarkpor",
+    job: "Junior Front-end Developer",
+    image: "../images/image-john.jpg",
+    description: "If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible.I now feel so confident about starting up as a professional developer."
+  }
+];
+
+
